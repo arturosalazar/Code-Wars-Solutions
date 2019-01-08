128fe69dfef9395960219d8707d239b9c3588ba5
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-07 20:36:03 -0600

    Update and rename Kata – 7kyu – Who let the dogs out? to 7kyu – who let the dogs out.js

--- 7kyu – who let the dogs out.js
+++ 7kyu – who let the dogs out.js
@@ -1,13 +1,11 @@
 /* 
-I wanted to write some classes for dogs. In my model dogs either bark or sleep. 
-I know that it is popular practice to set default values like this
+	I wanted to write some classes for dogs. In my model dogs either bark or sleep. 
+	I know that it is popular practice to set default values like this
 
-var value = value || DEFAULT_VALUE
+	var value = value || DEFAULT_VALUE
 
-And so i did. But something is wrong, and I don't know why. Can you help me out?
-*/
+	And so i did. But something is wrong, and I don't know why. Can you help me out?
 
-/*
 Starting Code:
 
 var BARK  = 'woof woof';
@@ -34,11 +32,10 @@ function dog_dont_bark_only_if_told_so(dont_bark){
 }
 */
 
-//My Solution
-
 var BARK  = 'woof woof';
 var SLEEP = 'zzzzzzzzz....';
 
+//use default parameters
 function dog_bark_by_default(bark = true){
   return bark ? BARK : SLEEP;
 }
