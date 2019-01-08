76570017813bea55c19d8c403e7bc785941e474c
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-01-07 20:37:53 -0600

    Update 7kyu – who let the dogs out.js

--- 7kyu – who let the dogs out.js
+++ 7kyu – who let the dogs out.js
@@ -1,10 +1,10 @@
 /* 
-	I wanted to write some classes for dogs. In my model dogs either bark or sleep. 
-	I know that it is popular practice to set default values like this
+I wanted to write some classes for dogs. In my model dogs either bark or sleep. 
+I know that it is popular practice to set default values like this
 
-	var value = value || DEFAULT_VALUE
+var value = value || DEFAULT_VALUE
 
-	And so i did. But something is wrong, and I don't know why. Can you help me out?
+And so i did. But something is wrong, and I don't know why. Can you help me out?
 
 Starting Code:
 
